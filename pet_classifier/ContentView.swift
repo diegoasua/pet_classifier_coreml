3adc14ec160ec900a9ec9c54755a188b5859e267
diegoasua <baxuaa@gmail.com>
2024-08-04 19:31:14 +0700

    Initial Commit

---
+++ pet_classifier/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pet_classifier
+//
+//  Created by Diego Asua on 8/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pet_classifier/pet_classifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  pet_classifierApp.swift
+//  pet_classifier
+//
+//  Created by Diego Asua on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct pet_classifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
